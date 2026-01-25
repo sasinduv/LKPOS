3439a9a8d5899e54e0b03730e4c4fec8a0ea86f8
sasinduv <sasindugihan123456@gmail.com>
2026-01-25 09:59:23 +0530

    add report to monthly sales barchart and piechart

--- src/main/java/lk/pos/controller/ReportController.java
+++ src/main/java/lk/pos/controller/ReportController.java
@@ -1,14 +1,20 @@
 package lk.pos.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.*;
 import javafx.scene.control.Label;
 import lk.pos.dao.ReportDAO;
+import lk.pos.util.ReportPDFGenerator;
+
+import java.util.Map;
 
 public class ReportController {
 
     @FXML private Label lblToday;
     @FXML private Label lblMonth;
     @FXML private Label lblQty;
+    @FXML private BarChart<String, Number> barChart;
+    @FXML private PieChart pieChart;
 
     @FXML
     public void initialize() {
@@ -16,10 +22,44 @@ public class ReportController {
             lblToday.setText("Rs. " + ReportDAO.getTodayeSales());
             lblMonth.setText("Rs. " + ReportDAO.getMonthlySales());
             lblQty.setText(String.valueOf(ReportDAO.getTotalSoldQty()));
+            loadBarchart();
+            loadPieChart();
 
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    private void loadBarchart() {
+        try {
+            XYChart.Series<String, Number> series = new XYChart.Series<>();
+            series.setName("Monthly Sales");
+
+            Map<String, Double> data = ReportDAO.getMonthlySalesChart();
+            data.forEach((month, total)->
+                    series.getData().add(new XYChart.Data<>(month, total))
+                    );
+            barChart.getData().add(series);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadPieChart() {
+        try {
+            Map<String, Integer> data = ReportDAO.getPaymentChart();
+            data.forEach((method, count) ->
+                    pieChart.getData().add(new PieChart.Data(method, count))
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void exportPDF() throws Exception {
+        ReportPDFGenerator.generate();
+
     }
 
 }
